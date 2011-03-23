0542f75b3ad6364111042556ba84d7f3ca90b6e7
James Harrison <james@balvedastudios.com>
2011-03-23 02:20:44 +0000

    Initial commit to rivendellrb.

---
+++ test/test_rivendellrb.rb
@@ -0,0 +1,7 @@
+require 'helper'
+
+class TestRivendellrb < Test::Unit::TestCase
+  should "probably rename this file and start testing for real" do
+    flunk "hey buddy, you should probably rename this file and start testing for real"
+  end
+end
